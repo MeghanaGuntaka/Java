4b074df694fc0e51c176203779d771b892bf7d47
MeghanaGuntaka <meghanareddy.guntaka343@gmail.com>
2024-08-12 16:24:01 +0530

    inital commit

--- eclipse-workspace/LambdaExpressions/src/TemperatureConverter/Conversion.java
+++ eclipse-workspace/LambdaExpressions/src/TemperatureConverter/Conversion.java
@@ -3,7 +3,6 @@ package TemperatureConverter;
 public class Conversion {
 	public static void main(String[] args) {
 		TempConverter temp=(double a)->{
-		
 			System.out.println("Celsius to Fahrenheit: "+ (a*9/5+32));
 			System.out.println("Fahrenheit to Celsius : "+ ((a-32)*(5/9)));
 		};
